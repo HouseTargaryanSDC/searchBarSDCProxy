a4c8617afb6d1b5679506deb4c58c0f06b192e71
Daniel Lin <daniellin215@gmail.com>
2019-01-23 19:43:00 -0800

    first commit

---
+++ proxy/index.js
@@ -0,0 +1,79 @@
+const axios = require('axios');
+const proxy = require('./proxy.js');
+
+const PORT = 9001;
+proxy.get('loaderio-02142c9541bdd340190a39242c2c527b.txt', function(req, res) {
+  res.sendFile('/Users/home/ubuntu/searchBarSDCProxy/public/loaderio-02142c9541bdd340190a39242c2c527b.txt')
+})
+
+proxy.get('/api/reviews/all/:restaurantId', (req, res) => {
+  console.log('proxy get reviews all');
+  const { restaurantId } = req.params;
+  axios.get(`http://54.183.243.96:80/api/reviews/all/${restaurantId}`)
+  // axios.get(`http://3.86.82.182:9001/api/reviews/all/${restaurantId}`)
+    .then(({data}) => {
+      res.status(200).send(data);
+    });
+});
+
+proxy.get('/api/reviews/summary/:restaurantId', (req, res) => {
+  const { restaurantId } = req.params;
+  axios.get(`http://54.183.243.96:80/api/reviews/summary/${restaurantId}`)
+  // axios.get(`http://3.86.82.182:9001/api/reviews/summary/${restaurantId}`)
+    .then(({data}) => {
+      res.status(200).send(data);
+    });
+});
+
+proxy.post('/api/reviews/all/:restaurantId', (req, res) => {
+  const { body } = req;
+  const { restaurantId } = req.params;
+  axios.post(`http://54.183.243.96:80/api/reviews/all/${restaurantId}`, body)
+  .then(() => {
+    res.status(201).send('post successful')
+  })
+  .catch((err) => {
+    console.log('error posting', err);
+    res.status(500).send('error posting!');
+  });
+});
+
+proxy.get('/api/nav/:metro', (req, res) => {
+  const { metro } = req.params;
+  axios.get(`http://127.0.0.1:9004/api/nav/${metro}`)
+  // axios.get(`http://3.86.82.182:9004/api/nav/${metro}`)
+    .then(({data}) => {
+      res.status(200).send(data);
+    });
+});
+
+proxy.get('/api/search/:searched', (req, res) => {
+  const { searched } = req.params;
+  axios.get(`http://54.183.64.166:9004/api/search/${searched}`)
+  // axios.get(`http://3.86.82.182:9004/api/search/${searched}`)
+    .then(({data}) => {
+      res.status(200).send(data);
+    });
+});
+
+proxy.get('/api/menus/:rest_id', (req, res) => {
+  console.log('proxy menu get request');
+  const { rest_id } = req.params;
+  axios.get(`http://127.0.0.1:9003/api/menus/${rest_id}`)
+  // axios.get(`http://3.86.82.182:9003/api/menus/${rest_id}`)
+    .then(({data}) => {
+      res.status(200).send(data);
+    });
+});
+
+proxy.get('/api/restaurant', (req, res) => {
+  axios.get(`http://127.0.0.1:9002/api/restaurant`, {params: {id: 2}})
+  // axios.get(`http://3.86.82.182:9002/api/restaurant`, {params: {id: 2}})
+    .then(({data}) => {
+      res.status(status).send(data);
+    });
+});
+
+proxy.listen(PORT, () => {
+  console.log('Proxy listening on PORT ', PORT);
+});
